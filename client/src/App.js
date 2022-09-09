80cca25dd14b597d393d6be580f552b092444912
hamza-ihind <ihind.mizo@gmail.com>
2022-09-09 18:09:07 +0100

    added navigation bar (logic + style)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import './App.scss';
+
+import { About, Testemonials, Work, Skills, Header, Footer } from './container';
+import { Navbar } from './components';
 
 function App() {
-	return <div className='App'>Hamza</div>;
+	return (
+		<div className='app'>
+			<Navbar />
+			<Header />
+			<About />
+			<Work />
+			<Skills />
+			<Testemonials />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
