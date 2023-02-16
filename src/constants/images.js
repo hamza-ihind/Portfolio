f66f788d36b36f3781324c27f96ca6658c5296c0
hamza-ihind <ihind.mizo@gmail.com>
2023-02-16 04:29:02 +0100

    slight changes to update

---
+++ src/constants/images.js
@@ -0,0 +1,101 @@
+import email from "../assets/email.png";
+import mobile from "../assets/mobile.png";
+import api from "../assets/api.png";
+import cpp from "../assets/cpp.png";
+import css from "../assets/css.png";
+import figma from "../assets/figma.png";
+import flutter from "../assets/flutter.png";
+import git from "../assets/git.png";
+import graphql from "../assets/graphql.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import mu5 from "../assets/mu5.png";
+import node from "../assets/node.png";
+import nodejs from "../assets/nodejs.jpeg";
+import python from "../assets/python.png";
+import react from "../assets/react.png";
+import reactjs from "../assets/reactjs.png";
+import redux from "../assets/redux.png";
+import sass from "../assets/sass.png";
+import typescript from "../assets/typescript.png";
+import vue from "../assets/vue.png";
+import uiux from "../assets/uiux.jpg";
+import xd from "../assets/xd.png";
+import csharp from "../assets/csharp.png";
+
+import about01 from "../assets/about01.png";
+import about02 from "../assets/about02.png";
+import about03 from "../assets/about03.png";
+import about04 from "../assets/about04.png";
+
+import profile from "../assets/profile.png";
+import profile2 from "../assets/profile2.png";
+import bahti from "../assets/bahti.jpeg";
+import hassan from "../assets/hassan.jpg";
+import circle from "../assets/circle.svg";
+import logo from "../assets/logo.png";
+import pattern from "../assets/pattern.svg";
+import quote from "../assets/quote.png";
+
+import adidas from "../assets/adidas.png";
+import amazon from "../assets/amazon.png";
+import asus from "../assets/asus.png";
+import bolt from "../assets/bolt.png";
+import nb from "../assets/nb.png";
+import skype from "../assets/skype.png";
+import spotify from "../assets/spotify.png";
+
+import clock from "../assets/clock.png";
+import todoist from "../assets/todoist.jpg";
+import ensa from "../assets/ensa.png";
+import compil from "../assets/compil.png";
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+  reactjs,
+  nodejs,
+  uiux,
+  profile2,
+  pattern,
+  csharp,
+  xd,
+  bahti,
+  quote,
+  clock,
+  hassan,
+  todoist,
+  ensa,
+  compil,
+};
